2537762155ff4d193e10d691d3b7e8f84d2e6293
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2026-03-31 10:42:22 +0200

    refactor: update Firebase configuration to use environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9zD79gr5OMNXM-Hl0Mxcd0i6L25q-u3M",
-  authDomain: "vs-web-studio.firebaseapp.com",
-  projectId: "vs-web-studio",
-  storageBucket: "vs-web-studio.firebasestorage.app",
-  messagingSenderId: "775581499549",
-  appId: "1:775581499549:web:f891390f0322b4c4f95474",
-  measurementId: "G-7MBJHSMV37"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
