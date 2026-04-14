026f58e53de3246536a65b0ee11d9101eb62b0af
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2026-04-14 20:53:08 +0000

    Deploying to gh-pages from @ Svitly4nyi-Vla2yslav/VS-Studio@f5444b154b89b0d807c7e48968017d11f59bd00b 🚀

--- assets/index-DqihZYov.js
+++ assets/index-DqihZYov.js
@@ -1,4 +1,4 @@
-import{u as s,j as e,P as a,A as d,a as o,d as r,m as l}from"./index-DMO2IDmk.js";const c=r.div`
+import{u as s,j as e,P as a,A as d,a as o,d as r,m as l}from"./index-CEHj0hBK.js";const c=r.div`
   padding: 24px 0 96px;
 `,g=r.section`
   display: grid;
