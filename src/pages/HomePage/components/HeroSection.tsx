93bcbce490e61626ce9d14414f6c8afadd84903a
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2026-05-22 16:22:35 +0200

    fix(HeroSection): remove unused icon imports

--- src/pages/HomePage/components/HeroSection.tsx
+++ src/pages/HomePage/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { backOut, easeOut, motion, useScroll, useTransform } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
-import { FaArrowRight, FaBolt, FaBullseye, FaEuroSign, FaIdCard, FaLifeRing, FaSearch } from 'react-icons/fa';
+import { FaArrowRight, FaBolt, FaBullseye, FaEuroSign, FaLifeRing, FaSearch } from 'react-icons/fa';
 import TerminalType from '../../../components/Motion/TerminalType';
 import HeroVideo from '../../../components/Motion/HeroVideo';
 import { revealUp } from './homePageData';
