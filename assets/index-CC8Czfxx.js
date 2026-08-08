9497c3221bba61a5ef33c6a9cbedbbffc0360061
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2026-08-08 13:18:26 +0000

    Deploying to gh-pages from @ Svitly4nyi-Vla2yslav/VS-Studio@7ece74c3cd4184625218bc8b16cf4c9a1c0f9243 🚀

--- assets/index-CC8Czfxx.js
+++ assets/index-CC8Czfxx.js
@@ -1,4 +1,4 @@
-import{u as s,j as e,P as a,A as d,a as o,g as r,m as l}from"./index-C3elIAi5.js";const c=r.div`
+import{u as s,j as e,P as a,A as d,a as o,g as r,m as l}from"./index-CDrMdaeK.js";const c=r.div`
   padding: 24px 0 96px;
 `,g=r.section`
   display: grid;
